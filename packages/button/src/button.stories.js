f0cca886b03aae304cff124890fe8fc6fe9f6abd
Ryosuke <ryosuke.san.hana@gmail.com>
2020-06-09 12:55:16 -0700

    test(button): storybook test for button
    Basic Storybook test for the button. Tests use of attribute.

---
+++ packages/button/src/button.stories.js
@@ -0,0 +1,9 @@
+import { html } from "lit-html";
+import "./button.js";
+
+export default {
+  title: "Button",
+};
+
+export const Button = () =>
+  html`<spooky-button type="submit">Submit</spooky-button>`;

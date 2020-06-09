dfb06777869b0d37bec8491716d7519bc15f738a
Ryosuke <ryosuke.san.hana@gmail.com>
2020-06-09 15:33:32 -0700

    refactor(button): import new hook for constructable stylesheets
    Imported and integrated hook that replaces same functionality inside component.

--- packages/button/src/button.js
+++ packages/button/src/button.js
@@ -1,6 +1,7 @@
 import { html } from "lit-html";
-import { css, unsafeCSS, supportsAdoptingStyleSheets } from "lit-element";
-import { component, useEffect } from "haunted";
+import { css, unsafeCSS } from "lit-element";
+import { component } from "haunted";
+import { useConstructableStylesheets } from "spooky-ui-hooks";
 
 export function Button({ type, color }) {
   const styles = [
@@ -11,45 +12,7 @@ export function Button({ type, color }) {
     `,
   ];
 
-  /**
-   * Applies styling to the element shadowRoot using the [[`styles`]]
-   * property. Styling will apply using `shadowRoot.adoptedStyleSheets` where
-   * available and will fallback otherwise. When Shadow DOM is polyfilled,
-   * ShadyCSS scopes styles and adds them to the document. When Shadow DOM
-   * is available but `adoptedStyleSheets` is not, styles are appended to the
-   * end of the `shadowRoot` to [mimic spec
-   * behavior](https://wicg.github.io/construct-stylesheets/#using-constructed-stylesheets).
-   */
-  const adoptStyles = () => {
-    if (styles.length === 0) {
-      return;
-    }
-    // There are three separate cases here based on Shadow DOM support.
-    // (1) shadowRoot polyfilled: use ShadyCSS
-    // (2) shadowRoot.adoptedStyleSheets available: use it
-    // (3) shadowRoot.adoptedStyleSheets polyfilled: append styles after
-    // rendering
-    if (window.ShadyCSS !== undefined && !window.ShadyCSS.nativeShadow) {
-      window.ShadyCSS.ScopingShim.prepareAdoptedCssText(
-        styles.map((s) => s.cssText),
-        this.localName
-      );
-    } else if (supportsAdoptingStyleSheets) {
-      this.shadowRoot.adoptedStyleSheets = styles.map((s) =>
-        s instanceof CSSStyleSheet ? s : s.styleSheet
-      );
-    } else {
-      styles.forEach((s) => {
-        const style = document.createElement("style");
-        style.textContent = s.cssText;
-        this.shadowRoot.appendChild(style);
-      });
-    }
-  };
-
-  useEffect(() => {
-    adoptStyles();
-  }, [styles]);
+  useConstructableStylesheets(this, styles);
 
   return html`
     <button type=${type ? type : "button"}>

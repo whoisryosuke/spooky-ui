ddb487e3a3872a3ff0040b738fb1627bba00c409
Ryosuke <ryosuke.san.hana@gmail.com>
2020-06-15 15:48:12 -0700

    feat(button): responsive utility props with arrays working
    Extended utility props to accept arrays. Creates media queries for each array value.

--- packages/button/src/button.js
+++ packages/button/src/button.js
@@ -5,14 +5,51 @@ import { convertProps } from 'utility-props';
 
 const namespace = 'spky';
 
-function createUtilityStyles(props) {
+function createMediaQuery(breakpoint) {
+  return `@media only screen and (min-width: ${unsafeCSS(breakpoint)})`;
+}
+
+function createUtilityStyles(
+  props,
+  breakpoints = ['400px', '800px', '1200px']
+) {
   let styles = css``;
+  let responsiveStyles = css``;
   const cssProperties = Object.keys(props);
   if (cssProperties && cssProperties.length > 0) {
     cssProperties.forEach(cssProperty => {
       const conversion = convertProps(cssProperty);
       if (props[cssProperty] !== undefined) {
-        const propValue = conversion(props[cssProperty], namespace);
+        // Handle array-based props
+        if (
+          Array.isArray(props[cssProperty]) &&
+          props[cssProperty].length > 0
+        ) {
+          props[cssProperty].forEach((responsiveProp, propIndex) => {
+            const propValue = conversion(responsiveProp, namespace);
+            // Generate a CSS media query based on array index
+            responsiveStyles = css`${responsiveStyles}\n ${unsafeCSS(
+              createMediaQuery(breakpoints[propIndex])
+            )} {\n :host {\n ${unsafeCSS(`${cssProperty}`)}: ${unsafeCSS(
+              propValue
+            )}; \n} \n}`;
+          });
+        }
+
+        // Handle non-responsive props
+        let propValue;
+        // We assign the lowest breakpoint as a backup property
+        // If the media queries override, they worked!
+        // If not, the component defaults to a "mobile-friendly" setting
+        if (
+          Array.isArray(props[cssProperty]) &&
+          props[cssProperty].length > 0
+        ) {
+          propValue = conversion(props[cssProperty][0], namespace);
+        } else {
+          propValue = conversion(props[cssProperty], namespace);
+        }
+
         styles = css`${styles}\n${unsafeCSS(`${cssProperty}:`)} ${unsafeCSS(
           propValue
         )};`;
@@ -21,13 +58,10 @@ function createUtilityStyles(props) {
   }
   return css`
     :host {
+      display: block;
       ${styles}
     }
-    button {
-      font-family: inherit;
-      font-weight: inherit;
-      color: inherit;
-    }
+    ${responsiveStyles}
   `;
 }
 
@@ -42,6 +76,9 @@ export function Button({ type, color, width, height, margin }) {
     css`
       button {
         width: 100%;
+        font-family: inherit;
+        font-weight: inherit;
+        color: inherit;
       }
     `,
   ];

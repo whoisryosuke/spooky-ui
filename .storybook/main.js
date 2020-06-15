e661b138828501ecfb1363f73958efd0ed2f519b
Ryosuke <ryosuke.san.hana@gmail.com>
2020-06-15 09:28:07 -0700

    chore: added OpenWC test/lint/demo
    Added Karma for testing, ESLint/Prettier, and better Storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,14 @@
 module.exports = {
-  stories: ["../packages/**/*.stories.js"],
+  stories: ['../packages/**/stories/**/*.stories.{js,md,mdx}'],
+  addons: [
+    'storybook-prebuilt/addon-knobs/register.js',
+    'storybook-prebuilt/addon-docs/register.js',
+    'storybook-prebuilt/addon-viewport/register.js',
+  ],
+  esDevServer: {
+    // custom es-dev-server options
+    nodeResolve: true,
+    watch: true,
+    open: true,
+  },
 };

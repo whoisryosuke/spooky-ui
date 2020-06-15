5324c8e65ad158cfc3b3364c5f7cc135db545edc
Ryosuke <ryosuke.san.hana@gmail.com>
2020-06-15 14:40:17 -0700

    chore: wrapped all stories in theme provider
    Added theme provider so all Storybook components receive CSS vars

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
-import { addParameters, setCustomElements } from '@open-wc/demoing-storybook';
+import { html } from 'lit-html';
+import {
+  addParameters,
+  addDecorator,
+  setCustomElements,
+} from '@open-wc/demoing-storybook';
+import '../packages/theme/theme-provider.js';
+
+const ThemeDecorator = storyFn =>
+  html`<spooky-theme>${storyFn()}</spooky-theme>`;
+
+addDecorator(ThemeDecorator);
 
 addParameters({
   docs: {
@@ -6,12 +17,12 @@ addParameters({
   },
 });
 
-async function run() {
-  const customElements = await (
-    await fetch(new URL('../custom-elements.json', import.meta.url))
-  ).json();
+// async function run() {
+//   const customElements = await (
+//     await fetch(new URL('../custom-elements.json', import.meta.url))
+//   ).json();
 
-  setCustomElements(customElements);
-}
+//   setCustomElements(customElements);
+// }
 
-run();
+// run();

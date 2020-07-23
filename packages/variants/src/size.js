b428d5263a2944fd8278766a45cb11337d864d76
Ryosuke <ryosuke.san.hana@gmail.com>
2020-07-23 14:26:22 -0700

    feat(variants): added all size variants
    Finished adding all 10 size variations from 0-9

--- packages/variants/src/size.js
+++ packages/variants/src/size.js
@@ -19,6 +19,70 @@ export default function size(prop) {
       `;
       break;
 
+    case '2':
+      variantStyles = css`
+        [size='2'] {
+          font-size: var(--spky-font-sizes-2);
+        }
+      `;
+      break;
+
+    case '3':
+      variantStyles = css`
+        [size='3'] {
+          font-size: var(--spky-font-sizes-3);
+        }
+      `;
+      break;
+
+    case '4':
+      variantStyles = css`
+        [size='4'] {
+          font-size: var(--spky-font-sizes-4);
+        }
+      `;
+      break;
+
+    case '5':
+      variantStyles = css`
+        [size='5'] {
+          font-size: var(--spky-font-sizes-5);
+        }
+      `;
+      break;
+
+    case '6':
+      variantStyles = css`
+        [size='6'] {
+          font-size: var(--spky-font-sizes-6);
+        }
+      `;
+      break;
+
+    case '7':
+      variantStyles = css`
+        [size='7'] {
+          font-size: var(--spky-font-sizes-7);
+        }
+      `;
+      break;
+
+    case '8':
+      variantStyles = css`
+        [size='8'] {
+          font-size: var(--spky-font-sizes-8);
+        }
+      `;
+      break;
+
+    case '9':
+      variantStyles = css`
+        [size='9'] {
+          font-size: var(--spky-font-sizes-9);
+        }
+      `;
+      break;
+
     default:
       break;
   }
